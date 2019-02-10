480f5241abbfc10a22a9c58b69d0508ef81fe2fe
hardrisk <hardrisk@users.noreply.github.com>
2019-02-10 23:04:31 +0300

    Add files via upload

---
+++ müzik/linkolustur.php
@@ -0,0 +1,33 @@
+<?php
+
+$az = ($_POST["name"]);
+$asd = ($_POST["email"]);
+$whois = "https://www.whois.com/whois/"; //çöp
+$trwhois = "http://www.whois.com.tr/domain/"; //çöp
+$isup = "http://isup.me/"; //çöp
+$vimeo = "https://player.vimeo.com/video/"; //çöp
+
+echo "<center>";
+echo "<textarea>,{'title':'" .$az;
+echo "','url':'" .$asd;
+echo "'}</textarea><br>";
+?>
+<html>
+<body bgcolor="gray">
+<style>
+input{;outline:none;text-align:center}
+textarea{font-size:80%;text-align:center;width:700px;height:100px;outline:none;border:2px solid black;z-index:1}
+</style>
+<center><br><br>
+<input type="button" title="sayfayı yenile" value="yenile" onclick="window.location.reload();"><br>
+<form style="margin-top:50px" id="jsform" action="" method="post">
+Title: <br><input size="40" autofocus type="text" name="name" placeholder="tırnak içermesin"><br>
+Link: <br><input size="40" type="text" name="email" placeholder="video linki"><br>
+<input type="submit" value="yolla">
+</form>
+<a href="mzk.php">mzk.php</a><br>
+
+</center>
+
+</body>
+</html>
